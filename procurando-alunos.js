67d037fc9edf378358dd4848b2de4578f26a2af9
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-06-08 15:21:09 +0000

    ;

---
+++ procurando-alunos.js
@@ -0,0 +1,10 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias]
+
+function exibeNomeENota(alunos){
+    if (listaDeAlunosEMedias[0].includes(aluno));
+    console.log(` `)
+
+}
